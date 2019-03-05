edac5854e79c1f56d59ab5ef4bb21faa458a0bb6
CShark <liuver.development@gmail.com>
2019-03-05 13:19:22 -0500

    de cshark
    arreglando tamanno de cadenas para las migraciones de mysql

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -5,6 +5,7 @@ namespace App\Providers;
 use Illuminate\Support\ServiceProvider;
 use Modules\Account\Domain\Repository\IRegisterUserRepository;
 use Mudules\Account\Infrastructure\Respository\Mysql\MysqlRegisterUserRepository;
+use Illuminate\Support\Facades\Schema;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -16,6 +17,7 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
         //
+        Schema::defaultStringLength(191);
     }
 
     /**
